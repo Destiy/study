c0cef65917304023943fa1869278e26e2fefd788
Destiy <1060778075@qq.com>
2020-04-24 11:02:03 +0800

    Update StLang.java
    long

--- src/stlang/StLang.java
+++ src/stlang/StLang.java
@@ -6,10 +6,13 @@ public class StLang {
     Integer integer;
 
     public static void main(String[] args) {
+//
+//        System.out.println(Integer.toBinaryString(Integer.MIN_VALUE));
+//        System.out.println(Integer.toBinaryString(-2147483648));
+//        System.out.println(Integer.toBinaryString(-8).toString());
+//        System.out.println(Integer.toBinaryString(8));
 
-        System.out.println(Integer.toBinaryString(Integer.MIN_VALUE));
-        System.out.println(Integer.toBinaryString(-2147483648));
-        System.out.println(Integer.toBinaryString(-8).toString());
-        System.out.println(Integer.toBinaryString(8));
+        int result = 1;
+        System.out.println(result << 1);
     }
 }
