02bdb9cfb5b50f71af67360f10e3ac3cf10039bf
Destiy <1060778075@qq.com>
2020-05-15 18:00:05 +0800

    Update ShortTest.java
    code type

--- src/ShortTest.java
+++ src/ShortTest.java
@@ -1,10 +1,16 @@
 public class ShortTest {
 
     public static void main(String[] args) {
-        short s1 = 1;
+//        short s1 = 1;
+////        s1 = s1 + 1;
+//        s1 += 1;
 
-//        s1 = s1 + 1;
-        s1 += 1;
-
+        String a = "hellow2";
+        final String b = "hellow";
+        String c = "hellow";
+        String d = b + 2;
+        String e = c + 2;
+        System.out.println(a == d);
+        System.out.println(a == e);
     }
 }
