089dcb2fbacd529d94732f8d0172f42fc6efd4e0
Destiy <1060778075@qq.com>
2020-06-29 01:51:00 +0800

    lcs

--- src/interview/I0628/Test1.java
+++ src/interview/I0628/Test1.java
@@ -1,8 +1,6 @@
 package interview.I0628;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * 请完成一个简单的算法编程。
@@ -16,15 +14,16 @@ import java.util.Stack;
  * @data 2020-06-28 20:58
  */
 public class Test1 {
-    private static List<String> result = new LinkedList<>();
+    private static List<StringBuffer> result = new LinkedList<>();
+    private static Set<Character> set;
 
     public static void main(String[] args) {
-        String str = "google";
-
+        String str = "abcda";
+        set = new HashSet<>(str.length());
         Test1 test1 = new Test1();
         StringBuilder sb = new StringBuilder(str);
         test1.getLCS(str, sb.reverse().toString());
-        result.forEach( x -> System.out.println(x + ""));
+        result.forEach(x -> System.out.println(x + ""));
     }
 
     public void getLCS(String str, String reStr) {
@@ -52,28 +51,50 @@ public class Test1 {
                 }
             }
         }
-        printLcs(b, str, str.length(), reStr.length());
-        String res = "";
-        while(!stack.empty()) {
-            res += stack.pop().toString();
-        }
-        result.add(res);
+        printLcs(b, str, str.length(), reStr.length(), false);
+//        String res = "";
+//        while(!stack.empty()) {
+//            res += stack.pop().toString();
+//        }
+//        result.add(res);
     }
 
     private Stack<Character> stack = new Stack<Character>();
 
-    private void printLcs(char b[][], String str, int i, int j) {
+    private void printLcs(char b[][], String str, int i, int j, boolean flag) {
         if (i == 0 || j == 0) {
+
         } else if (b[i][j] == 'x') {
-            stack.push(str.charAt(i - 1));
-            printLcs(b, str, i - 1, j - 1);
+            char c = str.charAt(i - 1);
+//            stack.push();
+            if (flag && set.size() > 0) {
+                StringBuffer s = result.get(0);
+                set.forEach(x -> result.add(s.append(x)));
+            } else if (result.size() == 0) {
+                result.add(new StringBuffer(c + ""));
+            } else result.forEach(res -> res.append(c));
+            printLcs(b, str, i - 1, j - 1, false);
         } else if (b[i][j] == 'h') {
-            printLcs(b, str, i - 1, j);
+            set.add(str.charAt(i - 1));
+            printLcs(b, str, i - 1, j, true);
         } else {
-            printLcs(b, str, i, j - 1);
+            set.add(str.charAt(i - 1));
+            printLcs(b, str, i, j - 1, true);
         }
     }
 
+
+    /*    private void printLcs2(char b[][], String str, int i, int j) {
+            if (i == 0 || j == 0) {
+            } else if (b[i][j] == 'x') {
+                stack.push(str.charAt(i - 1));
+                printLcs(b, str, i - 1, j - 1);
+            } else if (b[i][j] == 'h') {
+                printLcs(b, str, i - 1, j);
+            } else {
+                printLcs(b, str, i, j - 1);
+            }
+        }*/
     public int compare(int left, int right, int max, String str) {
         int len = str.length();
         int clen = 0;
