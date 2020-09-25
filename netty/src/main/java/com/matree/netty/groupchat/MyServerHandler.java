4d601f58f6e8ec366f0bdbc42c1d3e3ed5954a6b
wangyao@zhilunkeji.com <wangyao@zhilunkeji.com>
2020-09-25 18:04:18 +0800

    webSocket

---
+++ netty/src/main/java/com/matree/netty/groupchat/MyServerHandler.java
@@ -0,0 +1,37 @@
+package com.matree.netty.groupchat;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleStateEvent;
+
+/**
+ * @author wy
+ */
+public class MyServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * @param ctx 上下文
+     * @param evt 事件
+     * @throws Exception
+     */
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleStateEvent event = (IdleStateEvent) evt;
+            String eventType = null;
+            switch (event.state()) {
+                case READER_IDLE:
+                    eventType = "读空闲";
+                    break;
+                case WRITER_IDLE:
+                    eventType = "写空闲";
+                    break;
+                case ALL_IDLE:
+                    eventType = "度写空闲";
+                    break;
+                default:
+            }
+            System.out.println(ctx.channel().remoteAddress() + "--超时事件--" + eventType);
+        }
+    }
+}
