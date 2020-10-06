839724765a0a2eecf8b75fd191992a0804e14514
wy <1060778075@qq.com>
2020-10-06 17:21:38 +0800

    观察者模式

---
+++ headfirst/src/main/java/observer/weather/observer/impl/CurrentConditionsDisplay.java
@@ -0,0 +1,29 @@
+package observer.weather.observer.impl;
+
+import observer.weather.observer.DisplayElement;
+import observer.weather.observer.Observer;
+import observer.weather.observer.Subject;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private Subject weatherData;
+
+    public CurrentConditionsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: " + temperature
+                + "F degrees and " + humidity + "% humidity");
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.display();
+    }
+}
---
+++ headfirst/src/main/java/observer/weather/observer/impl/StatisticsDisplay.java
@@ -0,0 +1,38 @@
+package observer.weather.observer.impl;
+
+import observer.weather.observer.DisplayElement;
+import observer.weather.observer.Observer;
+import observer.weather.observer.Subject;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+    private float maxTemp = 0.0f;
+    private float minTemp = 200;
+    private float tempSum= 0.0f;
+    private int numReadings;
+    private Subject weatherData;
+
+    public StatisticsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Avg/Max/Min temperature = " + (tempSum / numReadings)
+                + "/" + maxTemp + "/" + minTemp);
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        tempSum += temp;
+        numReadings++;
+
+        if (temp > maxTemp) {
+            maxTemp = temp;
+        }
+        if (temp < minTemp) {
+            minTemp = temp;
+        }
+        this.display();
+    }
+}
